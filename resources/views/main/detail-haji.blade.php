5a3e8cec0268f31028e78eaffec7fb4d648e31df
fuadadhim24 <fuadadhim24@gmail.com>
2025-03-09 01:48:19 +0700

    refactor: bug layout

--- resources/views/main/detail-haji.blade.php
+++ resources/views/main/detail-haji.blade.php
@@ -69,37 +69,61 @@
         </div>
     </nav>
     <section class="xl:h-screen 2xl:h-96 flex flex-col justify-center items-center w-screen">
-        <div id="default-carousel" class="mt-24 w-full xl:hidden" data-carousel="static">
-            <!-- Carousel wrapper -->
-            <div class="relative h-56 overflow-hidden rounded-lg md:h-96 xl:hidden">
+        <div id="indicators-carousel" class="relative w-full" data-carousel="static">
+            <div class="flex w-full h-96 overflow-hidden rounded-lg ">
                 <!-- Item 1 -->
-                <div class="hidden duration-1000 ease-in-out" data-carousel-item>
-                    <img src="{{ Vite::asset('resources/images/lp-main/haji/detail-catalog-dummy-1.png') }}"
-                        class="absolute block h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <div class="hidden duration-700 ease-in-out w-full" data-carousel-item="active">
+                    <div class="h-full flex items-center justify-center ">
+                        <div class="h-full p-4 bg-white shadow-xl rounded-xl">
+                            <img src="{{ Vite::asset('resources/images/lp-main/haji/detail-catalog-dummy-1.png') }}"
+                                class="h-full rounded-xl" alt="...">
+
+                        </div>
+                    </div>
                 </div>
                 <!-- Item 2 -->
-                <div class="hidden duration-1000 ease-in-out" data-carousel-item>
-                    <img src="{{ Vite::asset('resources/images/lp-main/haji/detail-catalog-dummy-1.png') }}"
-                        class="absolute block h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                </div>
-                {{-- <!-- Item 3 -->
-                <div class="hidden duration-1000 ease-in-out" data-carousel-item>
-                    <img src="{{ Vite::asset('resources/images/lp-main/haji/detail-catalog-dummy-1.png') }}"
-                        class="absolute block h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <div class="hidden duration-700 ease-in-out w-full" data-carousel-item>
+                    <div class="h-full flex items-center justify-center ">
+                        <div class="h-full p-4 bg-white shadow-xl rounded-xl">
+                            <img src="{{ Vite::asset('resources/images/lp-main/haji/detail-catalog-dummy-1.png') }}"
+                                class="h-full rounded-xl" alt="...">
+
+                        </div>
+                    </div>
+                    </div>
+                    <!-- Item 3 -->
+                    <div class="hidden duration-700 ease-in-out w-full" data-carousel-item>
+                        <div class="h-full flex items-center justify-center ">
+                            <div class="h-full p-4 bg-white shadow-xl rounded-xl">
+                                <img src="{{ Vite::asset('resources/images/lp-main/haji/detail-catalog-dummy-1.png') }}"
+                                class="h-full rounded-xl" alt="...">
+
+                        </div>
+                    </div>
                 </div>
                 <!-- Item 4 -->
-                <div class="hidden duration-1000 ease-in-out" data-carousel-item>
-                    <img src="{{ Vite::asset('resources/images/lp-main/haji/detail-catalog-dummy-1.png') }}"
-                        class="absolute block h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                </div> --}}
+                <div class="hidden duration-700 ease-in-out w-full" data-carousel-item>
+                    <div class="h-full flex items-center justify-center ">
+                        <div class="h-full p-4 bg-white shadow-xl rounded-xl">
+                            <img src="{{ Vite::asset('resources/images/lp-main/haji/detail-catalog-dummy-1.png') }}"
+                                class="h-full rounded-xl" alt="...">
+
+                        </div>
+                    </div>
+                </div>
                 <!-- Item 5 -->
-                <div class="hidden duration-1000 ease-in-out" data-carousel-item>
-                    <img src="{{ Vite::asset('resources/images/lp-main/haji/detail-catalog-dummy-1.png') }}"
-                        class="absolute block h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <div class="hidden duration-700 ease-in-out w-full" data-carousel-item>
+                    <div class="h-full flex items-center justify-center ">
+                        <div class="h-full p-4 bg-white shadow-xl rounded-xl">
+                            <img src="{{ Vite::asset('resources/images/lp-main/haji/detail-catalog-dummy-1.png') }}"
+                                class="h-full rounded-xl" alt="...">
+
+                        </div>
+                    </div>
                 </div>
             </div>
             <!-- Slider indicators -->
-            <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+            <div class="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2">
                 <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"
                     data-carousel-slide-to="0"></button>
                 <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
@@ -116,7 +140,7 @@
                 class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
                 data-carousel-prev>
                 <span
-                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-red-primary dark:bg-red-primary group-hover:bg-red-primary dark:group-hover:bg-red-300 group-focus:ring-4 group-focus:ring-red-300 dark:group-focus:ring-red-300 group-focus:outline-none">
+                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-red-primary dark:bg-red-primary group-hover:bg-red-500 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                     <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -129,7 +153,7 @@
                 class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
                 data-carousel-next>
                 <span
-                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-red-primary dark:bg-red-primary group-hover:bg-red-primary dark:group-hover:bg-red-300 group-focus:ring-4 group-focus:ring-red-300 dark:group-focus:ring-red-300 group-focus:outline-none">
+                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-red-primary dark:red-primary group-hover:bg-red-500 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                     <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -137,7 +161,9 @@
                     </svg>
                     <span class="sr-only">Next</span>
                 </span>
-            </button>
+                </button>
+            </div>
+
         </div>
 
 
@@ -158,7 +184,7 @@
                 <img class="rounded-xl" src="{{ Vite::asset('resources/images/lp-main/haji/detail-catalog-dummy-1.png') }}" alt="">
             </div>
         </div>
-        
+
         <!-- Modal -->
         <div id="imageModal" class="modal hidden">
             <span class="close" onclick="closeModal()">&times;</span>
